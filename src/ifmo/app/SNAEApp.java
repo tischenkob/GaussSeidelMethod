147d89adb272a84a5139ef702647d5b08e68fac4
Bogdan <tischenkob@gmail.com>
2020-06-11 12:56:20 +0300

    some work on implementation

--- src/ifmo/app/SNAEApp.java
+++ src/ifmo/app/SNAEApp.java
@@ -1,12 +1,46 @@
 package ifmo.app;
 
 import ifmo.interact.Interviewer;
+import ifmo.math.equations.nonlinear.AbstractMethod;
+import ifmo.math.equations.nonlinear.FixedPointIteration;
+import ifmo.math.equations.nonlinear.NewtonMethod;
+import ifmo.misc.BiFunctionGroup;
+import ifmo.misc.FunctionGroup;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.function.Function;
 
 public class SNAEApp implements App {
+    static FunctionGroup func1 = new FunctionGroup(
+            x -> Math.exp(2 * x) + 3 * x - 4,
+            x -> 2 * Math.exp(2 * x) + 3,
+            x -> 4 * Math.exp(2 * x),
+            x -> Math.log(4 - 3 * x) / 2
+    );
+
+    static List<BiFunctionGroup> funcSys1 = new ArrayList<>(List.of(
+            new BiFunctionGroup(
+                    (x1, x2) -> x1 + x2,
+                    null, null,
+                    null
+            ),
+            new BiFunctionGroup(
+                    (x1, x2) -> x1 - x2,
+                    null,
+                    null,
+                    null)
+    ));
+    public static void main(final String[] args) {
+        AbstractMethod method = new FixedPointIteration(func1.getPhi());
+        double x = method.solve(0.475);
+        System.out.println(x);
+        method = new NewtonMethod(func1.getFunction(), func1.getDerivativeOne());
+        x = method.solve(0.475);
+        System.out.println(x);
+    }
     @Override
     public void start() {
         Interviewer interviewer = new Interviewer(new Scanner(System.in));
